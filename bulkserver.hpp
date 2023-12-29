66d928b28cf885497f27035119dccdb4251ce9d7
sergey <sgorcev84@mail.ru>
2023-12-29 21:34:02 +0300

    [*] Сетевая версия bulk с использованием асинхронных вызовов библиотеки asio.

--- bulkserver.hpp
+++ bulkserver.hpp
@@ -1,57 +1,73 @@
 #pragma once
 
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/detached.hpp>
-#include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/tcp.hpp>
-#include <boost/asio/signal_set.hpp>
-#include <boost/asio/write.hpp>
-#include <boost/asio/this_coro.hpp>
-#include <boost/asio/awaitable.hpp>
-#include <boost/asio/co_spawn.hpp>
-#include <boost/asio/use_awaitable.hpp>
+#include <asio.hpp>
+#include <async.h>
 #include <cstdio>
+#include <iostream>
 
-using boost::asio::ip::tcp;
-using boost::asio::awaitable;
-using boost::asio::co_spawn;
-using boost::asio::detached;
-using boost::asio::use_awaitable;
-namespace this_coro = boost::asio::this_coro;
+using asio::ip::tcp;
 
-// awaitable<void> echo_once(tcp::socket& socket)
-// {
-//   char data[128];
-//   std::size_t n = co_await socket.async_read_some(boost::asio::buffer(data), use_awaitable);
-//   co_await async_write(socket, boost::asio::buffer(data, n), use_awaitable);
-// }
-
-// awaitable<void> echo(tcp::socket socket)
-// {
-//   try
-//   {
-//     for (;;)
-//     {
-//       // The asynchronous operations to echo a single chunk of data have been
-//       // refactored into a separate function. When this function is called, the
-//       // operations are still performed in the context of the current
-//       // coroutine, and the behaviour is functionally equivalent.
-//       co_await echo_once(socket);
-//     }
-//   }
-//   catch (std::exception& e)
-//   {
-//     std::printf("echo Exception: %s\n", e.what());
-//   }
-// }
-
-awaitable<void> listener()
+///
+/// \brief The RaiiHelper class RAII - хэлпер для упрощщения работы с функциями библиотеки async
+///
+class AsyncHelper
 {
-  auto executor = co_await this_coro::executor;
-  tcp::acceptor acceptor(executor, {tcp::v4(), 55555});
-  for (;;)
-  {
-    tcp::socket socket = co_await acceptor.async_accept(use_awaitable);
-    co_spawn(executor, echo(std::move(socket)), detached);
-  }
+public:
+    AsyncHelper(std::size_t bulkCommandsLimit): m_handler {async::connect(bulkCommandsLimit)} {}
+    AsyncHelper(const AsyncHelper &) = delete;
+    AsyncHelper(AsyncHelper &&other) noexcept = default;
+    ~AsyncHelper() { async::disconnect(m_handler); }
+
+    AsyncHelper &operator=(const AsyncHelper &) = delete;
+    AsyncHelper &operator=(AsyncHelper &&) = default;
+
+    void receive(const char * const data, std::size_t size) const;
+
+private:
+    async::print_handler_t m_handler;
+
+};
+
+///
+/// \brief session_reader корутина для чтения из сокета
+/// \param socket - клиентский сокет
+/// \param bulkCommandsLimit размер блока команд
+/// \return
+///
+asio::awaitable<void> session_reader(tcp::socket socket, std::size_t bulkCommandsLimit)
+{
+    try {
+        auto clientSocket = std::move(socket);
+        auto asyncHandler = AsyncHelper {bulkCommandsLimit};
+        for (std::string read_msg;;) {
+            std::size_t n = co_await asio::async_read_until(clientSocket,
+                                                            asio::dynamic_buffer(read_msg, 1024), "\n", asio::use_awaitable);
+
+            asyncHandler.receive(read_msg.c_str(), n - 1);
+            read_msg.erase(0, n);
+        }
+    } catch (std::exception& e) {
+        std::cerr << "session_reader exception" << e.what() << std::endl;
+    }
+}
+
+///
+/// \brief listener Корутина ассинхроной обработки  входящих tcp-соединений
+/// \param executor Конктекст выыполнения
+/// \param serverPort  Порт TCP-сервера
+/// \param bulkCommandsLimit Размер блока команд
+/// \return
+///
+asio::awaitable<void> listener(asio::io_context &executor, std::uint16_t serverPort, std::size_t bulkCommandsLimit)
+{
+    tcp::acceptor acceptor(executor, {tcp::v6(), serverPort});
+    for (;;) {
+        tcp::socket socket = co_await acceptor.async_accept(asio::use_awaitable);
+        asio::co_spawn(executor, session_reader(std::move(socket), bulkCommandsLimit), asio::detached);
+    }
+}
+
+inline void AsyncHelper::receive(const char * const data, std::size_t size) const
+{
+    async::receive(m_handler, data, size);
 }
--- main.cpp
+++ main.cpp
@@ -14,37 +14,27 @@ int main(int argc, const char *argv[])
     std::cout << "Async app version: " << PROJECT_VERSION << std::endl;
 
     const auto bulkSize = argc;
-    if (bulkSize != 2) {
+    if (bulkSize != 3) {
         std::cout << "Usage:" << std::endl;
-        std::cout << argv[0] << " [number of commands]" << std::endl;
+        std::cout << argv[0] << "<por> <bulk size>" << std::endl;
         return 1;
     }
 
     // обработка параметра командной строки с числом команд в блоке
-    const int bulkCommandsLimit = std::atoi(argv[1]);
+    const std::uint16_t serverPort = std::atoi(argv[1]);
+
+    // обработка параметра командной строки с числом команд в блоке
+    const int bulkCommandsLimit = std::atoi(argv[2]);
     if (bulkCommandsLimit < 1)
         return 0;
 
-    // обработка набора команд
-    auto cinCmdProcessing = [bulkCommandsLimit]()
-    {
-        std::string strCommand;
-        auto connectHandler = async::connect(static_cast<std::size_t>(bulkCommandsLimit));
-
-        while (std::cin >> strCommand) {
-            async::receive(connectHandler, strCommand.c_str(), strCommand.size());
-        }
-
-        async::disconnect(connectHandler);
-    };
-
     try {
-        boost::asio::io_context io_context(1);
+        asio::io_context io_context(1);
 
-        boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+        asio::signal_set signals(io_context, SIGINT, SIGTERM);
         signals.async_wait([&](auto, auto){ io_context.stop(); });
 
-        co_spawn(io_context, listener(), detached);
+        asio::co_spawn(io_context, listener(io_context, serverPort, bulkCommandsLimit), asio::detached);
 
         io_context.run();
     }
